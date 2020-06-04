59d2e371aecefa6368f0c661d81fb7b89a3cb60f
Piyush Jagtap <piyush1997jagtap@gmail.com>
2020-06-05 00:44:15 +0530

    First commit

---
+++ conFusion/src/app/services/leader.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { from, } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+import { catchError } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  constructor(private http: HttpClient, private processHTTPMsgService: ProcessHTTPMsgService) { }
+  
+  getLeaders(): Observable<Leader[]> {
+    return this.http.get<Leader[]>(baseURL + 'leadership')
+    .pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+  getLeader(id: string): Observable<Leader> {
+    return this.http.get<Leader>(baseURL + 'leadership/' + id)
+    .pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+
+  getFeaturedLeader(): Observable<Leader> {
+    return this.http.get<Leader[]>(baseURL + 'leadership?featured=true').pipe(map(leadership => leadership[0]))
+    .pipe(catchError(this.processHTTPMsgService.handleError));
+  }
+}
